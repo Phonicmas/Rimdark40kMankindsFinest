7a35e82cf3dd6e69fac4e069c6f2b9bfaf46bd6f
Phonicmas <jkskovlund@gmail.com>
2024-06-25 18:20:30 +0200

    Add stuff from Factions mod and tinkered it

---
+++ 1.5/Source/Genes40k/HarmonyPatches/HarmonyPatch_KeepPrerequisitePermit.cs
@@ -0,0 +1,28 @@
+﻿using Core40k;
+using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+
+namespace Genes40k
+{
+    [HarmonyPatch(typeof(Pawn_RoyaltyTracker), "AddPermit")]
+    public class KeepPrerequisitePermit
+    {
+        public static void Postfix(RoyalTitlePermitDef permit, Faction faction, Pawn_RoyaltyTracker __instance)
+        {
+            if (!permit.HasModExtension<DefModExtension_KeepPrerequisitePermit>())
+            {
+                return;
+            }
+
+            if (permit.prerequisite == null)
+            {
+                return;
+            }
+            __instance.AllFactionPermits.Add(new FactionPermit(faction, __instance.GetCurrentTitle(faction), permit.prerequisite));
+        }
+    }
+}
