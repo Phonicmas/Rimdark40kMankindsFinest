3c866eb873d9e6033b3c819d2ade0b47201a7dd6
MewnGoddess <gabgossow@gmail.com>
2025-01-21 03:03:43 +0300

    Source files that belong to the dll we rolled back to

--- 1.5/Source/Genes40k/ChapterApparelColourTwo/PawnRenderNodeWorker_AttachmentShoulderPad.cs
+++ 1.5/Source/Genes40k/ChapterApparelColourTwo/PawnRenderNodeWorker_AttachmentShoulderPad.cs
@@ -7,6 +7,13 @@ namespace Genes40k
 {
     public class PawnRenderNodeWorker_AttachmentShoulderPad : PawnRenderNodeWorker
     {
+        /*public override Vector3 ScaleFor(PawnRenderNode node, PawnDrawParms parms)
+        {
+            var vector = base.ScaleFor(node, parms);
+            var bodyGraphicScale = parms.pawn.story.bodyType.bodyGraphicScale;
+            return vector * ((bodyGraphicScale.x + bodyGraphicScale.y) / 2f);
+        }*/
+        
         public override bool CanDrawNow(PawnRenderNode node, PawnDrawParms parms)
         {
             var pawn = parms.pawn;
