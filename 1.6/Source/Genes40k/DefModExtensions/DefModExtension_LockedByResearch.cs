5403c27f3ea9cecbd467d5ae2a8af77a1193d130
Phonicmas <jkskovlund@gmail.com>
2025-07-02 18:59:35 +0200

    new research to allow any flesh to be used when research is done

---
+++ 1.6/Source/Genes40k/DefModExtensions/DefModExtension_LockedByResearch.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace Genes40k;
+
+public class DefModExtension_LockedByResearch : DefModExtension
+{
+    public List<ResearchProjectDef> researchPrerequisites = new List<ResearchProjectDef>();
+}
---
+++ 1.6/Source/Genes40k/HarmonyPatches/HarmonyPatch_LockedByResearch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Genes40k;
+
+[HarmonyPatch(typeof(RecipeDef), "AvailableNow", MethodType.Getter)]
+public class HarmonyPatch_LockedByResearch
+{
+    public static bool Prefix(ref bool __result, RecipeDef __instance)
+    {
+        if (!__instance.HasModExtension<DefModExtension_LockedByResearch>())
+        {
+            return true;
+        }
+        
+        var defMod = __instance.GetModExtension<DefModExtension_LockedByResearch>();
+        foreach (var research in defMod.researchPrerequisites)
+        {
+            if (!research.IsFinished)
+            {
+                continue;
+            }
+            __result = false;
+            return false;
+        }
+
+        return true;
+    }
+}
