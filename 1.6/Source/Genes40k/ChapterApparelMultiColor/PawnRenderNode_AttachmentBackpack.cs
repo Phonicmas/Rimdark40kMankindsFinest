e0dbad886e1d748f678dec09a57adc6708dbf504
Phonicmas <jkskovlund@gmail.com>
2025-08-02 20:22:13 +0200

    mask fix backpack

--- 1.6/Source/Genes40k/ChapterApparelMultiColor/PawnRenderNode_AttachmentBackpack.cs
+++ 1.6/Source/Genes40k/ChapterApparelMultiColor/PawnRenderNode_AttachmentBackpack.cs
@@ -18,25 +18,54 @@ public class PawnRenderNode_AttachmentBackpack : PawnRenderNode_Apparel
     {
         var apparelMultiColor = (BodyDecorativeApparelMultiColor)apparel;
 
+        var texPath = Props.texPath;
         string maskPath = null;
 
-        if (apparelMultiColor.MaskDef != null && apparelMultiColor.MaskDef.maskExtraFlags.Contains("HasBackpack"))
-        {
-            maskPath = apparelMultiColor.MaskDef?.maskPath;
+        var jumpPackVisual = ModsConfig.RoyaltyActive &&
+                             pawn.apparel.WornApparel.Any(wornApparel =>
+                                 wornApparel.def == Genes40kDefOf.Apparel_PackJump) &&
+                             apparelMultiColor.def.HasModExtension<DefModExtension_HideJumpPack>() && apparelMultiColor
+                                 .def.GetModExtension<DefModExtension_HideJumpPack>().changeBackpackVisual;
 
-            if (maskPath != null)
+        if (jumpPackVisual)
+        {
+            texPath += "_Jump";
+        }
+
+        if (apparelMultiColor.MaskDef != null)
+        {
+            maskPath = apparelMultiColor.MaskDef.maskPath;
+            
+            var backpackMask = apparelMultiColor.MaskDef.maskExtraFlags.Contains("HasBackpack");
+            var jumppackMask = apparelMultiColor.MaskDef.maskExtraFlags.Contains("HasJumppack");
+            
+            if (jumpPackVisual)
             {
-                maskPath += "_Backpack";
+                if (maskPath != null && jumppackMask)
+                {
+                    maskPath += "_Backpack_Jump";
+                }
+                else
+                {
+                    maskPath = null;
+                }
             }
-            if (ModsConfig.RoyaltyActive && pawn.apparel.WornApparel.Any(wornApparel => wornApparel.def == Genes40kDefOf.Apparel_PackJump) && apparelMultiColor.def.HasModExtension<DefModExtension_HideJumpPack>() && apparelMultiColor.def.GetModExtension<DefModExtension_HideJumpPack>().changeBackpackVisual)
+            else
             {
-                maskPath += "_Jump";
+                if (maskPath != null && backpackMask)
+                {
+                    maskPath += "_Backpack";
+                }
+                else
+                {
+                    maskPath = null;
+                }
             }
         }
         
         var shader = Core40kDefOf.BEWH_CutoutThreeColor.Shader;
         
-        return MultiColorUtils.GetGraphic<Graphic_Multi>(Props.texPath, shader, Props.drawSize, apparelMultiColor.DrawColor, apparelMultiColor.DrawColorTwo, apparelMultiColor.DrawColorThree, apparelMultiColor.Graphic.data, maskPath);
+        return MultiColorUtils.GetGraphic<Graphic_Multi>(texPath, shader, Props.drawSize, apparelMultiColor.DrawColor, apparelMultiColor.DrawColorTwo, apparelMultiColor.DrawColorThree, apparelMultiColor.def.graphicData, maskPath);
     }
     
     protected override IEnumerable<Graphic> GraphicsFor(Pawn pawn)
