26df8da8cb827ffb7af32d2b145c3e63c53915d0
Phonicmas <jkskovlund@gmail.com>
2026-02-04 17:44:18 +0100

    smol addition

--- 1.6/Source/Genes40k/Rank/ChapterRankDef.cs
+++ 1.6/Source/Genes40k/Rank/ChapterRankDef.cs
@@ -1,8 +1,33 @@
-﻿using Core40k;
+﻿using System.Text;
+using Core40k;
+using Verse;
 
 namespace Genes40k;
 
 public class ChapterRankDef : RankDef
 {
     public ShoulderIconDef unlocksRankIcon = null;
+
+    public override string BuildRankBonusString(StringBuilder stringBuilder)
+    {
+        var result = base.BuildRankBonusString(stringBuilder);
+        if (unlocksRankIcon == null)
+        {
+            return result;
+        }
+
+        var shoulderIconUnlockStringBuilder = new StringBuilder();
+        shoulderIconUnlockStringBuilder.AppendLine("    " + unlocksRankIcon.LabelCap);
+        var shoulderIconUnlock = "BEWH.MankindsFinest.RankSystem.ShoulderIconUnlock".Translate() + "\n" + shoulderIconUnlockStringBuilder.ToString();
+        if (result.NullOrEmpty())
+        {
+            result = shoulderIconUnlock;
+        }
+        else
+        {
+            result += "\n" + shoulderIconUnlock;
+        }
+
+        return result;
+    }
 }
