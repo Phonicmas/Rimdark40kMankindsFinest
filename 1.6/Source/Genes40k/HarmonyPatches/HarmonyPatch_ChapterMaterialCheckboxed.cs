96ec383b67f80a2fa0e9ff0dff3e64618b136a50
Phonicmas <jkskovlund@gmail.com>
2026-02-14 16:53:00 +0100

    checkboxx material, move more comps

---
+++ 1.6/Source/Genes40k/HarmonyPatches/HarmonyPatch_ChapterMaterialCheckboxed.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Genes40k;
+
+[HarmonyPatch(typeof(TransferableUIUtility), "DoExtraIcons")]
+public static class ChapterMaterialCheckboxedPatch
+{
+    public static void Postfix(Transferable trad, Rect rect, ref float curX)
+    {
+        if (trad.AnyThing is not GeneMaterialExtra materialExtra)
+        {
+            return;
+        }
+
+        var maps = Find.Maps.Where(m => m.listerBuildings.ColonistsHaveBuilding(Genes40kDefOf.BEWH_SangprimusPortum));
+        var excludedMaterials = new List<ThingDef>();
+        foreach (var map in maps)
+        {
+            var building = (Building_SangprimusPortum)map.listerBuildings.AllBuildingsColonistOfDef(Genes40kDefOf.BEWH_SangprimusPortum).First();
+            excludedMaterials.AddRange(building.GetDirectlyHeldThings().Select(material => material.def));
+        }
+
+        if (!excludedMaterials.Contains(materialExtra.def))
+        {
+            return;
+        }
+        
+        var iconRect = new Rect(curX - 24f, (rect.height - 24f) / 2f, 24f, 24f);
+        GUI.DrawTexture(iconRect, Widgets.GetCheckboxTexture(true));
+        TooltipHandler.TipRegion(iconRect, "BEWH.MankindsFinest.Other.AlreadyUnlocked".Translate());
+        curX -= 24f;
+    }
+}
